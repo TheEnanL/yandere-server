03ed552b6ed6df654305469e7b603e1872849636
TheEnanL <120574691+TheEnanL@users.noreply.github.com>
2026-08-07 15:29:08 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs   = require('fs');
 const path = require('path');
 const { WebSocketServer } = require('ws');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ---------- HTTP сервер ----------
 const MIME = {
